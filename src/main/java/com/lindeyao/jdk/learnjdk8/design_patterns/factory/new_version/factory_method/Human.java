76d938c8019d89027842949540c347db5e6579f2
lindeyao <lindeyao.lingyiai.com>
2019-11-13 12:39:12 +0800

    工厂方法模式总结

--- src/main/java/com/lindeyao/jdk/learnjdk8/design_patterns/factory/new_version/factory_method/Human.java
+++ src/main/java/com/lindeyao/jdk/learnjdk8/design_patterns/factory/new_version/factory_method/Human.java
@@ -1,7 +1,7 @@
 package com.lindeyao.jdk.learnjdk8.design_patterns.factory.new_version.factory_method;
 
 /**
- * 人类（抽象出来），相当于抽象产品
+ * 人类（抽象出来），相当于抽象出某一种产品
  *
  * @author DeeLin
  * @create 2019-11-12-22:37
