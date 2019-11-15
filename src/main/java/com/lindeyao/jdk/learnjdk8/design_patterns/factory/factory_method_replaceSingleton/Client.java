0aaf5e376715813127872db4d442705bfd63bedb
lindeyao <lindeyao.lingyiai.com>
2019-11-15 16:40:05 +0800

    工厂方法模式扩展--替换单例模式

---
+++ src/main/java/com/lindeyao/jdk/learnjdk8/design_patterns/factory/factory_method_replaceSingleton/Client.java
@@ -0,0 +1,18 @@
+package com.lindeyao.jdk.learnjdk8.design_patterns.factory.factory_method_replaceSingleton;
+
+/**
+ * 场景调用类，让工厂方法模式替换单例模式
+ *
+ * @author ldy
+ * @create 2019-11-15 16:11
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        // 获取创建单例实例的工厂，直接获取实例
+        Singleton singleton = SingletonFactory.getSingleton();
+        // 单例执行方法
+        singleton.methoeInvoke();
+    }
+
+}
---
+++ src/main/java/com/lindeyao/jdk/learnjdk8/design_patterns/factory/factory_method_replaceSingleton/Singleton.java
@@ -0,0 +1,20 @@
+package com.lindeyao.jdk.learnjdk8.design_patterns.factory.factory_method_replaceSingleton;
+
+/**
+ * 单例实体（工厂方法扩展--替代单例模式）
+ *
+ * @author ldy
+ * @create 2019-11-15 15:22
+ */
+public class Singleton {
+
+    // 构造方法私有化，外部无法正常new该对象
+    private Singleton(){
+
+    }
+
+    // 业务方法
+    public void methoeInvoke() {
+        System.out.println("私有类方法执行...");
+    }
+}
