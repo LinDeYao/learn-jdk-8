80c70cf316de5c85b39c3a561dc9c0f62cedd2e7
lindeyao <lindeyao.lingyiai.com>
2020-01-09 16:24:56 +0800

    模板方法模式--银行业务办理例子

---
+++ src/main/java/com/lindeyao/jdk/learnjdk8/design_patterns/template/bank_bussiness/Client.java
@@ -0,0 +1,20 @@
+package com.lindeyao.jdk.learnjdk8.design_patterns.template.bank_bussiness;
+
+/**
+ * 模板方法--场景调用类
+ *
+ * @author ldy
+ * @create 2020-01-09 15:42
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        AbstractBankBussinessProcess bankBussiness_1 = new BankBusinesser_1();
+        // 人员1办理业务
+        bankBussiness_1.bankBussinessProcess();
+        // 人员2办理业务
+        AbstractBankBussinessProcess bankBussiness_2 = new BankBusinesser_2();
+        bankBussiness_2.bankBussinessProcess();
+    }
+
+}
