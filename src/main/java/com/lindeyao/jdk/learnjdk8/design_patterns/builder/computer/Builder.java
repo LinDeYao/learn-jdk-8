3f43b16bb6dfc252f68c9a177221317b43625b6f
LinDeYao <826929816@qq.com>
2020-01-13 22:28:18 +0800

    建造者模式学习：例子，电脑装机

---
+++ src/main/java/com/lindeyao/jdk/learnjdk8/design_patterns/builder/computer/Builder.java
@@ -0,0 +1,16 @@
+package com.lindeyao.jdk.learnjdk8.design_patterns.builder.computer;
+
+/**
+ * 建造者模式例子--电脑抽象建造者
+ * 至于安装什么型号的cpu，主板都不是我关心的，而是具体建造者关心的
+ * @author DeeLin
+ * @create 2020-01-13-22:04
+ */
+public interface Builder {
+    // 创建cpu
+    public void createCpu(String cpu);
+    // memory
+    public void createMemory(String memory);
+    // 组装电脑
+    Computer createComputer();
+}
