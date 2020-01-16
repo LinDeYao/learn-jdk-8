971516129a3fd4e6d289d104867609c4de6b2252
lindeyao <lindeyao.lingyiai.com>
2020-01-16 10:52:12 +0800

    java8时间API学习--LocalDate

--- src/main/java/com/lindeyao/jdk/learnjdk8/timeutil/LocalDateTest.java
+++ src/main/java/com/lindeyao/jdk/learnjdk8/timeutil/LocalDateTest.java
@@ -1,7 +1,12 @@
 package com.lindeyao.jdk.learnjdk8.timeutil;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.IsoFields;
+import java.time.temporal.TemporalField;
 
 /**
  * java8专注处理日期--LocalDate
@@ -24,5 +29,28 @@ public class LocalDateTest {
         System.out.println("LocalDate.of 显示指定年月日：" + localDateOf);
         LocalDate localDateOfYearDay = LocalDate.ofYearDay(2020, 15);
         System.out.println("localDate.ofYearDay获取指定年份的第15天：" + localDateOfYearDay);
+        LocalDate localDateOfEpochDay = LocalDate.ofEpochDay(-10L);  // 格林时区：1970-01-01
+        System.out.println("LocalDate.ofEpochDay指定相对于格林零时区的日偏移量：" + localDateOfEpochDay);
+
+        System.out.println("getYear()方法，获取年份信息：" + localDate.getYear());
+        System.out.println("getDayOfMonth()方法，获取当前天是月份的第几天：" + localDate.getDayOfMonth());
+        System.out.println("getDayOfWeek()方法，获取当前天是星期几：" + localDate.getDayOfWeek());
+        System.out.println("isLeapYear()方法，当前年份是不是闰年：" + localDate.isLeapYear());
+        System.out.println("lengthOfYear()方法，当前年份有多少天：" + localDate.lengthOfYear());
+
+        // 格式化日期时间格式：DateTimeFormatter
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        System.out.println("localDate.format()方法，格式化时间为yyyy/MM/dd：" + localDate.format(format));
+
+        // 设置对应日期对应部分(年月日，星期，月份的第几天)
+        System.out.println("withDayOfMonth()，设置成月份的第28天:" + localDate.withDayOfMonth(28));
+
+        // 日期相加减
+        LocalDate localDatePlusDays = localDate.plusDays(1L);
+        System.out.println("plusDays(),日期加上1天(值为负数则是减掉1天)：" + localDatePlusDays);
+
+        // 日期比较
+        boolean isAfter = localDatePlusDays.isAfter(localDate);
+        System.out.println("isAfter(),时间相比较,第二天是否比今天大：" + isAfter);
     }
 }
