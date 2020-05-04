11445d1009e42973862fab462a86e04914c48502
lindeyao <lindeyao.lingyiai.com>
2020-05-04 10:11:26 +0800

    Serializable接口：只作为的标识的作用

--- src/main/java/com/lindeyao/jdk/learnjdk8/transient_demo/User.java
+++ src/main/java/com/lindeyao/jdk/learnjdk8/transient_demo/User.java
@@ -6,12 +6,13 @@ import java.io.Serializable;
 
 /**
  * 用户
- *
+ * Serializable 接口之所以定义为空，是因为它只起到了一个标识的作用，告诉程序实现了它的对象是可以被序列化的
+ * 但真正序列化和反序列化的操作并不需要它来完成
  * @author ldy
  * @create 2019-09-29 10:21
  */
 @Data
-public class User implements Serializable{
+public class User implements Serializable {
 
     private static final long serialVersionUID = 8294180014912103005L;
 
